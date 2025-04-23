a6a2237aff1bc0645afab86036b2275e2f612bd4
vinaypandey1991 <vinaykp1991@gmail.com>
2025-04-23 16:54:45 +0530

    google console

--- app/layout.js
+++ app/layout.js
@@ -3,19 +3,32 @@ import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import Whatsapp from "@/components/Whatsapp/Whatsapp";
 import VisitorCounter from "@/components/VisitorCounter/VisitorCounter";
+import Script from "next/script";
 
 export const metadata = {
   title: "Beveromatic Vending Machines",
-  description:"One of the best Beveromatic 2 Lane, 3 Lane, 4 Lane Hot Beverage Vending Machine Available Here ",
+  description: "One of the best Beveromatic 2 Lane, 3 Lane, 4 Lane Hot Beverage Vending Machine Available Here ",
   icons: {
-    icon: "/favicon.ico", 
-    apple: "/apple-touch-icon.png", 
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
   },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+       <meta name="google-site-verification" content="sAP_mBFe-1gOVyAsKRigxAI2pPK8hMv571HbhtLq7Y8" />
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-295P5LR5MX"></Script>
+        <Script>
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-295P5LR5MX');`}
+        </Script>
+
+      </head>
       <body>
         <Header />
         {children}
